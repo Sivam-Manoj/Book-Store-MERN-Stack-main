6af4984ac19389fecb4870045e9d1e4504410787
Sivam Manoj <151199957+Sivam-Manoj@users.noreply.github.com>
2024-06-05 23:04:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 
 const app = express();
 const mongo_url = process.env.MONGO_URL
-
+const port = process.env.PORT
 
 // Middleware for parsing request body
 app.use(express.json());
