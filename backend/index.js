a12786c0a38c58ee06bd39e4eda1f346abace076
Sivam Manoj <151199957+Sivam-Manoj@users.noreply.github.com>
2024-06-06 00:08:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,9 @@ app.use("/books", booksRoute);
 
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  app.use(express.static(path.join(__dirname, "../backend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../backend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   });
 }
 
