8c46e54476571dc3c067e765c26fb3de7ea8ddca
Sivam Manoj <151199957+Sivam-Manoj@users.noreply.github.com>
2024-06-05 22:58:51 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
+    axios.get(`${process.env.BASE_URL}/books/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
